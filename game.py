ba8cbc3a53b80b4b97785015557c840b6bc39af9
Oky Mikhael <okymikhael@gmail.com>
2021-07-24 21:39:11 +0700

    Repair spacing pep8

--- game.py
+++ game.py
@@ -2,13 +2,16 @@ import random
 import string
 import time
 
+
 def takeSecond(elem):
     return elem[1]
+
 
 step = 0
 five_fastest = []
 three_fastest = []
-tamiya_and_eye = list((list(string.ascii_uppercase)[x], random.randrange(10, 100, 5)) for x in range(0, 25))
+tamiya_and_eye = list((list(string.ascii_uppercase)[
+                      x], random.randrange(10, 100, 5)) for x in range(0, 25))
 
 print("Tamiya Speed")
 print(tamiya_and_eye, "\n")
@@ -16,7 +19,7 @@ print(tamiya_and_eye, "\n")
 print("Winner Selection: ")
 for i in range(0, len(tamiya_and_eye)):
     if tamiya_and_eye[i] not in three_fastest:
-        three_fastest.append(tamiya_and_eye[i]) 
+        three_fastest.append(tamiya_and_eye[i])
     print(three_fastest)
     if(len(three_fastest) < 5):
         time.sleep(0.5)
@@ -25,7 +28,7 @@ for i in range(0, len(tamiya_and_eye)):
         print(three_fastest)
         three_fastest.sort(key=takeSecond)
         three_fastest = three_fastest[:3]
-        step+=1
+        step += 1
 
 print("\nWe have 3 fastest tamiya:")
 print(three_fastest)
@@ -35,7 +38,7 @@ print("Removing 3 fastest tamiya for finding another 2 tamiya\n")
 for i in three_fastest:
     tamiya_and_eye.remove(i)
 # print(tamiya_and_eye)
-        
+
 time.sleep(2)
 print("Test 2 Beginning in")
 time.sleep(0.5)
@@ -48,7 +51,7 @@ time.sleep(1)
 
 for i in range(0, len(tamiya_and_eye)):
     if tamiya_and_eye[i] not in five_fastest:
-        five_fastest.append(tamiya_and_eye[i]) 
+        five_fastest.append(tamiya_and_eye[i])
     print(five_fastest)
     if(len(five_fastest) < 5):
         time.sleep(0.5)
@@ -57,11 +60,11 @@ for i in range(0, len(tamiya_and_eye)):
         print(five_fastest)
         five_fastest.sort(key=takeSecond)
         five_fastest = five_fastest[:2]
-        step+=1
-        
+        step += 1
+
 five_fastest.sort(key=takeSecond)
 five_fastest = three_fastest + five_fastest[:2]
-step+=1
+step += 1
 
 print("Five fastest tamiya:", five_fastest)
 print("Much step for finding 5 fastest tamiya:", step)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 import string
 
+
 def takeSecond(elem):
     return elem[1]
+
 
 step = 0
 five_fastest = []
 three_fastest = []
-tamiya_and_eye = list((list(string.ascii_uppercase)[x], random.randrange(10, 100, 5)) for x in range(0, 25))
+tamiya_and_eye = list((list(string.ascii_uppercase)[
+                      x], random.randrange(10, 100, 5)) for x in range(0, 25))
 
 print("Tamiya Eyes")
 print(tamiya_and_eye)
@@ -16,13 +19,13 @@ print("End Tamiya Eyes\n")
 print("----Race Begin----")
 for i in range(0, len(tamiya_and_eye)):
     if tamiya_and_eye[i] not in three_fastest:
-        three_fastest.append(tamiya_and_eye[i]) 
+        three_fastest.append(tamiya_and_eye[i])
     if(len(three_fastest) < 5):
         continue
     else:
         three_fastest.sort(key=takeSecond)
         three_fastest = three_fastest[:3]
-        step+=1
+        step += 1
 
 print("Three fastest tamiya:", three_fastest)
 print("Much step for finding 3 fastest tamiya: ", step, "\n")
@@ -31,19 +34,19 @@ print("Removing 3 fastest tamiya for finding another 2 tamiya\n")
 for i in three_fastest:
     tamiya_and_eye.remove(i)
 # print(tamiya_and_eye)
-        
+
 for i in range(0, len(tamiya_and_eye)):
     if tamiya_and_eye[i] not in five_fastest:
-        five_fastest.append(tamiya_and_eye[i]) 
+        five_fastest.append(tamiya_and_eye[i])
     if(len(five_fastest) < 5):
         continue
     else:
         five_fastest.sort(key=takeSecond)
         five_fastest = five_fastest[:2]
-        step+=1
-        
+        step += 1
+
 five_fastest.sort(key=takeSecond)
-step+=1
+step += 1
 
 five_fastest = three_fastest + five_fastest[:2]
 print("Five fastest tamiya:", five_fastest)
